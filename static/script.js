0b178556bd2c1b03f9a8cb619c76774a393fab4c
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-04-21 12:32:04 -0400

    fix duplicate word count

--- static/script.js
+++ static/script.js
@@ -18,21 +18,24 @@ async function handleSubmit(evt) {
   if (!guess) return;
 
   if (words.includes(guess)) {
-    showMessage(`Already found ${guess}`, "error");
+    showMessage(`Already found ${guess.toUpperCase()}.`, "error");
+    $("#guess").val("");
+    return;
   }
   //   check server for validity
   const resp = await axios.get("/check-guess", { params: { guess: guess } });
   if (resp.data.result === "not-word") {
-    showMessage(`${guess.toUpperCase()} is not in our dictionary`, "error");
+    showMessage(`${guess.toUpperCase()} is not in our dictionary.`, "error");
   } else if (resp.data.result === "not-on-board") {
     showMessage(
-      `${guess.toUpperCase()} is not a valid word on this board`,
+      `${guess.toUpperCase()} is not a valid word on this board.`,
       "error"
     );
   } else {
     showMessage(`${guess.toUpperCase()} scored ${guess.length} points!`, "ok");
     score += guess.length;
     updateScore();
+    words.push(guess);
   }
   $("#guess").val("");
 }
